1f44b371ef0d5e1673c29e4ee428ef679514bca0
ApoorvaMbackup <apoorva.m@mailmywork.com>
2025-06-30 14:58:33 -0400

    Add Bolt Logo with Hyperlink

--- src/components/PoweredByBolt.tsx
+++ src/components/PoweredByBolt.tsx
@@ -10,23 +10,23 @@ const PoweredByBolt: React.FC<PoweredByBoltProps> = ({
   position = 'bottom-right' 
 }) => {
   const positionClasses = {
-    'bottom-right': 'fixed bottom-4 right-4',
-    'bottom-left': 'fixed bottom-4 left-4',
-    'bottom-center': 'fixed bottom-4 left-1/2 transform -translate-x-1/2',
+    'bottom-right': 'fixed bottom-4 right-4 z-50',
+    'bottom-left': 'fixed bottom-4 left-4 z-50',
+    'bottom-center': 'fixed bottom-4 left-1/2 transform -translate-x-1/2 z-50',
     'footer': 'relative'
   };
 
   return (
     <a
-      href="https://bolt.new"
+      href="https://bolt.new/"
       target="_blank"
       rel="noopener noreferrer"
-      className={`inline-block transition-all duration-300 hover:opacity-90 ${positionClasses[position]} ${className}`}
+      className={`inline-block transition-all duration-300 hover:opacity-90 hover:scale-105 ${positionClasses[position]} ${className}`}
     >
       <img 
-        src="/black_circle_360x360.png" 
+        src="/black_circle_360x360 copy.png" 
         alt="Powered by Bolt" 
-        className="w-12 h-12 rounded-full shadow-md hover:shadow-lg transition-shadow"
+        className="w-12 h-12 rounded-full shadow-lg hover:shadow-xl transition-shadow duration-300"
       />
     </a>
   );
